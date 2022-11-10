8e3bdcb66e2bc7a5fc72144c5740d10888cac2b7
sulau1 <99355115+sulau1@users.noreply.github.com>
2022-11-10 04:11:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-function formatDate(date) {
-  var dd = date.getDate();
-
-  var mm = date.getMonth() + 1;
-
-  var y = date.getFullYear();
-
-  return dd + "/" + mm + "/" + y;
-}
-function formatTime(time) {
-  var h = time.getHours();
-
-  var m = time.getMinutes();
-
-  var s = time.getSeconds();
-
-  return h + ":" + m + ":" + s;
-}
-var t = new Date();
-var d = new Date();
+import chalk from "chalk";
+chalk.level = 1;
 console.log(
-  "Current date is",
-  formatDate(d),
-  " and current time is",
-  formatTime(t)
+  "Date and time is " + chalk.yellow(new Date().toLocaleString("en-RU"))
 );
